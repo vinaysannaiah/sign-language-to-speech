541a8f80b6abc513aca0c03c8d5e4e12220802ed
vinaysannaiah <36826364+vinaysannaiah@users.noreply.github.com>
2019-05-09 20:19:13 +0200

    update-camera

--- record_gestures.py
+++ record_gestures.py
@@ -9,3 +9,11 @@ import numpy as np
 """
 cam = cv2.VideoCapture(0)
 
+if cam.isOpened():
+	ret, img = cam.read()
+	img = cv2.flip(img, 1)
+	cv2.rectangle(img, pt1=(300, 300), pt2=(100, 100), color=(0, 255, 0), thickness=1)
+	cv2.imshow("camera", img)
+	cv2.waitKey(0)
+
+
