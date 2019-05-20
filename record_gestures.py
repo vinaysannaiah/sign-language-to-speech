88673045109106c3826698f24febf171a00811d5
vinaysannaiah <36826364+vinaysannaiah@users.noreply.github.com>
2019-05-20 22:28:42 +0200

    "small modifications"

--- record_gestures.py
+++ record_gestures.py
@@ -10,12 +10,12 @@ import numpy as np
 cam = cv2.VideoCapture(0)
 
 if cam.isOpened():
-	# read the frames for m the camera
-	ret, img = cam.read()
+	# read the frames from the Camera
+	_, img = cam.read()
 	# flip the image for the convenience (optional)
 	img = cv2.flip(img, 1)
 	# draw a rectangle randomly on the image
-	cv2.rectangle(img, pt1=(300, 300), pt2=(0, 0), color=(0, 255, 0), thickness=1)
+	cv2.rectangle(img, pt1=(300, 300), pt2=(0, 0), color=(0, 255, 0), thickness=2)
 	# take that rectangle as the region of interest
 	roi = img[0:300, 0:300]
 	# convert the roi to gray scale for ease of processing
@@ -26,7 +26,13 @@ if cam.isOpened():
 	_, contours, hierarchy = cv2.findContours(edges.copy(),
 											  cv2.RETR_TREE,
 											  cv2.CHAIN_APPROX_SIMPLE)
+	print(contours)
+	# cv2.imshow("img", contours)
+	# cv2.waitKey(0)
+	# find the length of the contours
 	cont_len = len(contours)
+	print(cont_len)
+	# create a matrix of zeros in the shape of cont_len
 	area = np.zeros(cont_len)
 	for i in range(0, cont_len):
 		area[i] = cv2.contourArea(contours[i])
